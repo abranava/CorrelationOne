49e09afd081d5c019579088d2f38fa2f567e1d20
abranava <74986044+abranava@users.noreply.github.com>
2023-10-16 13:32:03 -0700

    Update script.js
    Dark/Light Theme

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('toggleButton');
+toggleButton.addEventListener('click', () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+});
