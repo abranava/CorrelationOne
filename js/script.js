e4e1c0e9b1b8b86bf2bca5548c5b224efd9f6754
abranava <74986044+abranava@users.noreply.github.com>
2023-10-16 20:43:00 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ toggleButton.addEventListener('click', () => {
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
     document.documentElement.setAttribute('data-theme', newTheme);
 });
+// Add an event listener to trigger the effect when the page loads
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the section to animate
+    const section = document.querySelector('.section');
+    
+    // Apply the fade-and-slide-in effect
+    section.style.opacity = '1';
+    section.style.transform = 'translateY(0)';
+});
