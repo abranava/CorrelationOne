36ded3d7107fc51b34672ba34beb59540d14230d
abranava <74986044+abranava@users.noreply.github.com>
2023-10-16 21:31:06 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 const toggleButton = document.getElementById('toggleButton');
 toggleButton.addEventListener('click', () => {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-theme', newTheme);
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  document.documentElement.setAttribute('data-theme', newTheme);
 });
 
+// Initialize the Intersection Observer
 document.addEventListener('DOMContentLoaded', function () {
   const sections = document.querySelectorAll('.resume-section, .introduction, .about-me, .site-description, .blog-post, .call-to-action');
+  
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = '1';
+        entry.target.style.transform = 'translateY(0)';
+        observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+  
   sections.forEach(section => {
-    section.style.opacity = '1';
-    section.style.transform = 'translateY(0)';
+    observer.observe(section);
   });
 });
-
