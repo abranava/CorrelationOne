f4bdd9c0faeec8061b7229423e72cadd4c1d236c
abranava <74986044+abranava@users.noreply.github.com>
2023-11-06 15:43:42 -0700

    Update script.js
    Added input validation and submission

--- js/script.js
+++ js/script.js
@@ -15,3 +15,43 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }, 1000); // Change 100 to control the delay, in milliseconds
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('contactForm');
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const messageInput = document.getElementById('message');
+  const submitBtn = document.getElementById('submitBtn');
+  const successMessage = document.getElementById('successMessage');
+
+  const checkInputValidity = () => {
+    let isValid = nameInput.checkValidity() && emailInput.checkValidity() && messageInput.checkValidity();
+    submitBtn.disabled = !isValid;
+    return isValid;
+  };
+
+  const toggleErrorMessage = (input) => {
+    const errorSpan = input.nextElementSibling;
+    if (!input.checkValidity()) {
+      errorSpan.style.display = 'inline';
+    } else {
+      errorSpan.style.display = 'none';
+    }
+  };
+
+  form.addEventListener('input', function () {
+    toggleErrorMessage(nameInput);
+    toggleErrorMessage(emailInput);
+    toggleErrorMessage(messageInput);
+    checkInputValidity();
+  });
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (checkInputValidity()) {
+      // Assuming here you would normally have code to actually submit the form
+      // For example, via AJAX or by sending it to a server-side script
+      successMessage.style.display = 'block';
+    }
+  });
+});
